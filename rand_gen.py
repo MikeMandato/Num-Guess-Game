731f8c67a219802c8af71cfae2016cd31fd7e491
Peter Vuyk <peter.vuyk456@gmail.com>
2024-12-08 16:50:06 -0500

    Rename all variables and functions to snake_case
    - Match PEP 8 standards

--- rand_gen.py
+++ rand_gen.py
@@ -3,51 +3,51 @@ import random
 min = int
 max = int
 
-def NumGame(min, max):
+def number_guessing_game(min, max):
     print("Welcome to the number game :)")
     print(f"Enter a number between {min} and {max}!")
 
-    GameInt = random.randint(1,500) ## How do I set this so that i only have to declare the numbers when I call the function?
+    number_to_guess = random.randint(1,500) ## How do I set this so that i only have to declare the numbers when I call the function?
     attempts = 0
 
     while True:
         attempts += 1
         try:
-            UserGuess = int(input("Enter your guess:"))
+            user_guess = int(input("Enter your guess:"))
         except ValueError:
-            print(f"Ooops, you entered {UserGuess}, which is not a number. Try again.")
+            print(f"Ooops, you entered {user_guess}, which is not a number. Try again.")
             continue
         # What would attempts start at if it was not stated above on line 11?  
-        if UserGuess == GameInt:
-            print(f"Congrats!!! \nYou guessed '{GameInt}' in {attempts} attempts!")
+        if user_guess == number_to_guess:
+            print(f"Congrats!!! \nYou guessed '{number_to_guess}' in {attempts} attempts!")
             break
-        elif abs(UserGuess - GameInt) <= 10:
-            if UserGuess < GameInt:
+        elif abs(user_guess - number_to_guess) <= 10:
+            if user_guess < number_to_guess:
                 print("SUPER CLOSE! But still too low")
             else:
                 print("SUPER CLOSE! But still too high")
-        elif abs(UserGuess - GameInt) <= 100:
-            if UserGuess < GameInt:
+        elif abs(user_guess - number_to_guess) <= 100:
+            if user_guess < number_to_guess:
                 print("Close, but still too low!")
             else:
                 print("Close, but still too high")
         else:
-            if UserGuess < GameInt:
+            if user_guess < number_to_guess:
                 print("Wayyy too low")
             else:
                 print("Wayyy too high")
 
-    PlayAgain = input("Would you like to play again? (y/n): ").lower()
-    if PlayAgain[0] == "n":
+    play_again = input("Would you like to play again? (y/n): ").lower()
+    if play_again[0] == "n":
         print("\nThanks for playing!\n")
 
 # Need to fix the problem of a int or incorrect string breaking the loop!
 
-#if UserGuess != int:
+#if user_guess != int:
 #print("""Please enter a NUMBER. 
 #Examples of numbers: 234, 401, 29, 1, 111, 197 
 #Now don't f*ck this up. Enter NUMBER here: """)
 #Could also put part of the print in the input :slight_smile:
-#UserGuess = int(input())
+#user_guess = int(input())
 
-NumGame(1, 500)
+number_guessing_game(1, 500)
